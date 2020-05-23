21a2acc18d9ab34f4d38c91555fbbdee44fcc4e1
CarlosPerez02 <carlos.perez.moreno.02@gmail.com>
2020-05-23 11:50:43 +0200

    feat: Input added. Styles initiated

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Searcher from './components/Searcher'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1> Film Database</h1>
+      </header>
+      <main>
+        <Searcher />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Searcher/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Searcher () {
+    return (
+        <section className='searchBox-wrap'>
+            <input type='text' placeholder='Search for a film...' className='search-box' />
+
+        </section>
+    )
+}
+
+export default Searcher
